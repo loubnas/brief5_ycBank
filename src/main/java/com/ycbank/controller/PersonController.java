01d7f3c3b9f5b0dde634e2b1352c37587b07a160
loubnas <loubnasoussi15489@gmail.com>
2021-12-21 17:04:35 +0100

    brief5

---
+++ src/main/java/com/ycbank/controller/PersonController.java
@@ -0,0 +1,13 @@
+package com.ycbank.controller;
+
+public class PersonController {
+// Ajout d'un eleve (ajout du compte )
+    //*** données eleve
+    //*** données de compte -- optionnels
+
+
+    //Transaction
+    //**** payee tous le monde
+// payee un coatch (avec id)
+// payee etudiant
+}
